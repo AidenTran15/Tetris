ec6989450b5584fd00d69f544df68b729b9148af
AidenTran15 <aidenkiettran@gmail.com>
2020-05-28 10:34:12 +1000

    lastest update

--- main.py
+++ main.py
@@ -170,8 +170,13 @@ def valid_space(shape, grid):
       return True
 
 
-def check_lost():
-      pass
+def check_lost(positions):
+      for pos in positions:
+            x, y = pos
+            if y < 1:
+                  return True
+            
+      return False
 
 
 def get_shape():
@@ -229,9 +234,21 @@ def main(win):
       current_piece = get_shape()
       next_piece = get_shape()
       clock = pygame.time.Clock()
-      fall_time = 0    
+      fall_time = 0  
+      fall_speed = 0.27  
 
       while run:
+            grid = create_grid(locked_positions)
+            fall_time += clock.get_rawtime()
+            clock.tick()
+
+            if fall_time/1000 > fall_speed:
+                  fall_time = 0
+                  current_piece.y += 1
+                  if not(valid_space(current_piece, grid)) and current_piece.y > 0:
+                        current_piece.y -= 1
+                        change.piece = True
+
             for event in pygame.event.get():
                   if event.type == pygame.QUIT:
                         run = False
@@ -254,7 +271,27 @@ def main(win):
                               if not(valid_space(current_piece, grid)):
                                     current_piece -= 1
 
+            shape_pos = convert_shape_format(current_piece)
+
+            for i in range(len(shape_pos)):
+                  x, y = shape_pos[i]
+                  if y > -1:
+                        grid[y][x] = current_piece.color
+
+            if change_piece:
+                  for pos in shape_pos:
+                        p = (pos[0], pos[1])
+                        locked_positions[p] = current_piece.color
+                  current_piece = next_piece
+                  next_piece = get_shape()
+                  change_piece = False
+
             draw_window(win, grid)
+
+            if check_lost(locked_positions):
+                  run = False
+            
+      pygame.display.quit()
 
 
 def main_menu(win):
